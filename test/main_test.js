e65175de0f4e53e438accece81df21408289ca29
osamakhanedu <osamakhanedu@gmail.com>
2021-02-04 12:56:25 +0500

    mocha setup and tested

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+
+// Test Suite
+describe("Mocha", () => {
+  // Test spec (unit test )
+  it("should run tests using npm ", () => {
+    expect(true).to.be.ok;
+  });
+});
